a5a389ae897189d4b707897df1df128d0025e6f5
emyfer <ema.bradic@fer.unizg.hr>
2026-01-24 21:51:33 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App() {
 
   return (
     <div className="App">
+      <img src='coffee_image.png' alt='coffe mug'></img>
       <h1>Pomodoro Focus</h1>
       <Timer onComplete={handleSessionComplete} />
       <Stats sessions={sessions} />
-      <img src='coffee_image.png'></img>
     </div>
   );
 }
